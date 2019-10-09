ec06932b7e85c587279f41c785405440d4b300dd
Edward Lach <edwarddlach@gmail.com>
2019-10-08 21:35:12 -0400

    initial files needed for sign in

--- lambda-api/sign-in/src/main/java/signIn/SignIn.java
+++ lambda-api/sign-in/src/main/java/signIn/SignIn.java
@@ -6,6 +6,7 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 public class SignIn implements RequestHandler<Integer, String> {
     public String handleRequest(Integer myCount, Context context) {
+
         LambdaLogger logger = context.getLogger();
         logger.log("received : " + myCount);
         return String.valueOf(myCount);
